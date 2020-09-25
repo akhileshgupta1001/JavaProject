6e75234fd74e4e77cc1f597c06872a3abe308197
Akhilesh Kumar Gupta <40818447+akhileshKnoldus@users.noreply.github.com>
2020-09-25 22:28:55 +0530

    spring-mvc-using service layer

---
+++ akhilesh/src/main/java/com/akhilesh/dao/EmployeeDAO.java
@@ -0,0 +1,17 @@
+package com.akhilesh.dao;
+
+import java.util.List;
+
+import com.akhilesh.entity.Employee;
+
+public interface EmployeeDAO {
+
+	public List<Employee> findAll();
+
+	public Employee findById(int theId);
+
+	public void save(Employee theEmployee);
+
+	public void deleteById(int theId);
+
+}
